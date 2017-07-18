319cdc46c317c562cbddf4ee078d4f885b080071
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-18 10:10:53 -0400

    coins hash is happy

--- cc_mob.rb
+++ cc_mob.rb
@@ -1,21 +1,21 @@
 def make_change(amount)
 	exchange = {}
 	coins = {"quarter" => 25, "dime" => 10, "nickel" => 5, "penny" => 1}
-	if amount >= quarter
-		exchange["quarter"] = amount / quarter
-		amount = amount % quarter
+	if amount >= coins["quarter"]
+		exchange["quarter"] = amount / coins["quarter"]
+		amount = amount % coins["quarter"]
 	end
-	if amount >= dime
-		exchange["dime"] = amount / dime
-		amount = amount % dime
+	if amount >= coins["dime"]
+		exchange["dime"] = amount / coins["dime"]
+		amount = amount % coins["dime"]
 	end
-	if amount >= nickel
-		exchange["nickel"] = amount / nickel
-		amount = amount % nickel
+	if amount >= coins["nickel"]
+		exchange["nickel"] = amount / coins["nickel"]
+		amount = amount % coins["nickel"]
 	end
-	if amount >= penny
-		exchange["penny"] = amount / penny
-		amount = amount % penny
+	if amount >= coins["penny"]
+		exchange["penny"] = amount / coins["penny"]
+		amount = amount % coins["penny"]
 	end
 exchange
 end
