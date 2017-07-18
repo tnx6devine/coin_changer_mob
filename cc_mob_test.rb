3e6c9964974288ea7c0a06244f189a6b62f2b60f
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-18 09:16:37 -0400

    test that 0 equals empty hash error

--- cc_mob_test.rb
+++ cc_mob_test.rb
@@ -2,8 +2,12 @@ require "minitest/autorun"
 require_relative "cc_mob.rb"
 
 class TestCC_Mob < Minitest::Test
+	def test_assert_that_1_equals_1
+		assert_equal(1, 1)
+	end
 
-
-
+	def test_assert_that_0_equals_empty_hash
+		assert_equal({}, make_change(0))
+	end
 
 end
