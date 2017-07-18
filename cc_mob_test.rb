02108fc533458fda00f5cee2e79be8bc3c76e68e
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-18 09:52:26 -0400

    test for dimes, failure

--- cc_mob_test.rb
+++ cc_mob_test.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require_relative "cc_mob.rb"
 
 class TestCC_Mob < Minitest::Test
-	
+
 	def test_assert_that_1_equals_1
 		assert_equal(1, 1)
 	end
@@ -25,5 +25,9 @@ class TestCC_Mob < Minitest::Test
 
 	def test_assert_that_8_cents_equals_1_nickel_3_pennies
 		assert_equal({"penny" => 3, "nickel" => 1}, make_change(8))
+	end
+
+	def test_assert_that_10_cents_equals_1_dime
+		assert_equal({"dime" => 1}, make_change(10))
 	end
 end
