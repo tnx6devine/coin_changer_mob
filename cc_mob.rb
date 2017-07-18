e516d6d4b0ec0d1acb2ef94b1b7fbd85da98ba6c
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-18 10:14:25 -0400

    multiple hashes for coins and exchange complete

--- cc_mob.rb
+++ cc_mob.rb
@@ -1,21 +1,11 @@
 def make_change(amount)
 	exchange = {}
 	coins = {"quarter" => 25, "dime" => 10, "nickel" => 5, "penny" => 1}
-	if amount >= coins["quarter"]
-		exchange["quarter"] = amount / coins["quarter"]
-		amount = amount % coins["quarter"]
+	coins.each do |coin, value|
+		if amount >= value
+			exchange[coin] = amount / value
+			amount = amount % value
+		end
 	end
-	if amount >= coins["dime"]
-		exchange["dime"] = amount / coins["dime"]
-		amount = amount % coins["dime"]
-	end
-	if amount >= coins["nickel"]
-		exchange["nickel"] = amount / coins["nickel"]
-		amount = amount % coins["nickel"]
-	end
-	if amount >= coins["penny"]
-		exchange["penny"] = amount / coins["penny"]
-		amount = amount % coins["penny"]
-	end
-exchange
+	exchange
 end
